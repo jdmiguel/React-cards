4bf357233e4a189522444e1dd98b7e4268846c5d
A. J. Soria R <ajsoriar@gmail.com>
2018-07-31 12:10:07 +0200

    Some changes 2 (#1)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-console.log("Hello Andres!");
+console.log("Hello Andres! 2");
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
